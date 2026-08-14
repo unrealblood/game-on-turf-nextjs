68331018f24fb2ebdf8d25728880f517968c14d5
Shubham Singh <shubham110084@outlook.com>
2026-08-14 11:22:36 +0530

    create auth/login and auth/register dummy routes and also empty components folder

---
+++ app/auth/login/page.js
@@ -0,0 +1,7 @@
+export default async function Login() {
+    return (
+        <div>
+            This is Login page.
+        </div>
+    );
+}
---
+++ app/auth/register/page.js
@@ -0,0 +1,7 @@
+export default async function Register() {
+    return (
+        <div>
+            This is Register page.
+        </div>
+    );
+}

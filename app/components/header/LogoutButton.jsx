858e2c4cb37bb57d02da324dcd81382c83e43cba
Shubham Singh <shubham110084@outlook.com>
2026-08-15 12:38:22 +0530

    add firebase signout functionality

--- app/components/header/LogoutButton.jsx
+++ app/components/header/LogoutButton.jsx
@@ -1,5 +1,25 @@
+import { app } from "@/lib/firebase/app";
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function LogoutButton() {
+    const [loading, setLoading] = useState(false);
+
+    const router = useRouter();
+    
+    async function handleLogout() {
+        setLoading(true);
+        
+        const auth = getAuth(app);
+        await signOut(auth);
+
+        setLoading(false);
+
+        router.push("/auth/login");
+    }
+
     return (
-        <button type="button" className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">Logout</button>
+        <button type="button" className={`${loading ? `bg-gray-200 text-black`: `bg-red-500 text-white`} px-4 py-2 rounded-md cursor-pointer`} onClick={handleLogout}>{loading ? "Logout..." : "Logout"}</button>
     );
 }

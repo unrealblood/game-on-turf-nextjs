ac7fffdd89578df727ab614dd3aa1a3b0dcfa195
Shubham Singh <shubham110084@outlook.com>
2026-08-15 10:08:35 +0530

    refine login page ui and add login form to it

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,9 +1,11 @@
+import LoginForm from "@/app/components/auth/login/LoginForm";
+
 export default async function Login() {
     return (
         <div className="flex-1 overflow-y-auto">
             <div className="w-[600px] mx-auto flex justify-center items-center mt-8 border border-gray-200 rounded-md p-4">
-                <div>
-                    <header className="flex justify-center items-center gap-8 flex-col">
+                <div className="w-full">
+                    <header className="flex justify-center items-center gap-4 flex-col mt-4">
                         <div className="bg-green-200 text-center p-2 rounded-full w-16">
                             <span className="bi-trophy text-2xl text-teal-500" />
                         </div>
@@ -12,7 +14,7 @@ export default async function Login() {
                     </header>
 
                     {/* Login Form */}
-                    
+                    <LoginForm />
                 </div>
             </div>
         </div>
---
+++ app/components/auth/login/LoginForm.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+
+export default function LoginForm() {
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log("Login successfull.");
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col justify-start items-start gap-6 mt-8 px-8">
+            <input type="email" placeholder="Enter your email address" className="border border-gray-200 p-2 rounded-md w-full" />
+
+            <input type="password" placeholder="Enter your password" className="border border-gray-200 p-2 rounded-md w-full" />
+
+            <button type="submit" className="cursor-pointer bg-green-500 text-white w-full px-8 py-2 rounded-md">Login</button>
+
+            <div className="text-center w-full text-teal-500">
+                <Link href={"/auth/register"}>Don't have an account. Register here.</Link>
+            </div>
+        </form>
+    );
+}

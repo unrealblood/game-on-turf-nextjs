9b9be1b05024c2a5b5b902d27a32d4e55a8727eb
Shubham Singh <shubham110084@outlook.com>
2026-08-19 09:17:08 +0530

    create list-ground page with header

---
+++ app/list-ground/page.jsx
@@ -0,0 +1,20 @@
+export default async function() {
+    return (
+        <div className="flex-1 overflow-y-auto mt-18">
+            <section className="flex justify-center items-center mt-4">
+                <div className="border border-gray-200 w-[600px] rounded-md">
+                    <header className="rounded-t-md bg-teal-500 text-white px-8 py-12">
+                        <div className="flex justify-start items-center gap-2">
+                            <span className="bi-plus-circle text-2xl" />
+                            <h1 className="text-2xl font-bold">List your Ground</h1>                            
+                        </div>
+
+                        <p className="text-white/70">Partner with us to manage bookings and increase your turf's visibility.</p>
+                    </header>
+
+                    
+                </div>
+            </section>
+        </div>
+    );
+}

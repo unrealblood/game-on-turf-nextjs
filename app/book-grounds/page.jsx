e68441006578c3bafbfc0bb7bf81f11c7f99372f
Shubham Singh <shubham110084@outlook.com>
2026-08-21 18:26:07 +0530

    add book-grounds page with title

---
+++ app/book-grounds/page.jsx
@@ -0,0 +1,10 @@
+export default async function BookGround() {
+    return (
+        <div className="flex-1 overflow-y-auto mt-18">
+            <section className="p-8">
+                <h1 className="font-bold text-2xl">Available Grounds</h1>
+                <p className="mt-2 text-gray-500">Find the perfect turf for your next match.</p>
+            </section>
+        </div>
+    );
+}
